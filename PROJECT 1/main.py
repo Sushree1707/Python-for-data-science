984907673d87697b0e8d3eb9e4bab97c11240b09
Sushree S S Sudha <137508497+Sushree1707@users.noreply.github.com>
2025-06-12 00:26:22 +0530

    Update main.py

--- PROJECT 1/main.py
+++ PROJECT 1/main.py
@@ -1,4 +1,4 @@
-#We all have played snake, water gun game in our childhood. If you haven’t, google the rules of this game and write a python program capable of playing this game with the user.
+#We all have played stone,paper and scissors game in our childhood. If you haven’t, google the rules of this game and write a python program capable of playing this game with the user.
 import random
 
 '''
